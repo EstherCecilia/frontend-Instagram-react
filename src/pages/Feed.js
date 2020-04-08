e18cfe93890e982350fe0c5c049491623b29c14f
Esther Cecilia <estherazapfy@gmail.com>
2020-04-08 13:36:13 -0300

    A

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -4,11 +4,12 @@ import more from "../assets/more.svg";
 import like from "../assets/like.svg";
 import comment from "../assets/comment.svg";
 import send from "../assets/send.svg";
-import cat from "../assets/cat.jpg";
+import {post} from "./Post.js"
 
 class Feed extends Component {
   render() {
     return (
+      post().map(post =>
       <section id="post-list">
         <article>
           <header>
@@ -16,56 +17,27 @@ class Feed extends Component {
               className="user-info"
               style={{ display: "flex", flexDirection: "column" }}
             >
-              <span>Esther Cecília</span>
-              <span className="place">Belo Horizonte</span>
+              <span>{post.nome}</span>
+              <span className="place">{post.cidade}</span>
             </div>
 
             <img src={more} />
           </header>
 
-          <img src={cat} />
+          <img src={post.foto} />
           <footer>
             <div className="actions">
-              <img src={like} />
+              <img onClick={()=>console.log("UAU")}src={like} />
               <img src={comment} />
               <img src={send} />
             </div>
-            <strong>900 curtidas</strong>
-            <p>
-              Um post muito massa de um gatinho!<span>#react</span>
+            <strong>{post.curtidas}</strong>
+            <p>{post.texto}<span>#react</span>
               <span>#cat</span>
             </p>
           </footer>
         </article>
-
-        <article>
-          <header>
-            <div
-              style={{ display: "flex", flexDirection: "column" }}
-              className="user-info"
-            >
-              <span>Esther Cecília</span>
-              <span className="place">Belo Horizonte</span>
-            </div>
-
-            <img src={more} />
-          </header>
-
-          <img src={cat} />
-          <footer>
-            <div className="actions">
-              <img src={like} />
-              <img src={comment} />
-              <img src={send} />
-            </div>
-            <strong>900 curtidas</strong>
-            <p>
-              Um post muito massa de um gatinho!<span>#react</span>
-              <span>#cat</span>
-            </p>
-          </footer>
-        </article>
-      </section>
+      </section>)
     );
   }
 }
---
+++ src/pages/Post.js
@@ -0,0 +1,98 @@
+import cat from "../assets/Cats/cat.jpg";
+import cat2 from "../assets/Cats/cat2.jpg";
+import cat3 from "../assets/Cats/cat3.jpg";
+import cat4 from "../assets/Cats/cat4.jpg";
+import cat5 from "../assets/Cats/cat5.jpg";
+import cat6 from "../assets/Cats/cat6.jpg";
+import cat7 from "../assets/Cats/cat7.jpg";
+import cat8 from "../assets/Cats/cat8.jpg";
+import cat9 from "../assets/Cats/cat9.jpg";
+import cat10 from "../assets/Cats/cat10.jpg";
+
+
+export function post() {
+  return [
+    {
+      id:1,
+      nome: "Esther Cecília",
+      cidade: "Belo Horizonte",
+      foto: cat,
+      curtidas: 900,
+      texto: "Um post muito massa de um gatinho!",
+    },
+    {
+        id:2,
+        nome: "Dandara Vitoria",
+        cidade: "Belo Horizonte",
+        foto: cat2,
+        curtidas: 850,
+        texto: "Um post muito massa de um gatinho!",
+      },
+      {
+        id:3,
+        nome: "Maria Antonis",
+        cidade: "Rio janeiro",
+        foto: cat3,
+        curtidas: 700,
+        texto: "Um post muito massa de um gatinho!",
+      },
+      {
+        id:4,
+        nome: "Pedro Sampaio",
+        cidade: "Belo Horizonte",
+        foto: cat4,
+        curtidas: 900,
+        texto: "Um post muito massa de um gatinho!",
+      },
+      {
+        id:5,
+        nome: "Cecília Maria",
+        cidade: "Belo Horizonte",
+        foto: cat5,
+        curtidas: 80,
+        texto: "Um post muito massa de um gatinho!",
+      },
+      {
+        id:6,
+        nome: "Esther Maria",
+        cidade: "Belo Horizonte",
+        foto: cat6,
+        curtidas: 80,
+        texto: "Um post muito massa de um gatinho!",
+      },
+      {
+        id:7,
+        nome: "Julia Cecília",
+        cidade: "São Paulo",
+        foto: cat7,
+        curtidas: 900,
+        texto: "Um post muito massa de um gatinho!",
+      },
+      
+      {
+        id:8,
+        nome: "Julia Nicoli",
+        cidade: "São Paulo",
+        foto: cat8,
+        curtidas: 900,
+        texto: "Um post muito massa de um gatinho!",
+      },
+      
+      {
+        id:9,
+        nome: "Pedro Cecílio",
+        cidade: "São Paulo",
+        foto: cat9,
+        curtidas: 900,
+        texto: "Um post muito massa de um gatinho!",
+      },
+      
+      {
+        id:10,
+        nome: "Cassandra Silva",
+        cidade: "São Paulo",
+        foto: cat10,
+        curtidas: 900,
+        texto: "Um post muito massa de um gatinho!",
+      }
+  ];}
